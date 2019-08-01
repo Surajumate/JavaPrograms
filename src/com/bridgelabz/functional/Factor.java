0b2926ef6d545314418fe36cac303efb275e61f3
Surajumate <surajumate1172@gmail.com>
2019-08-01 13:26:32 +0530

    suraj done with programs

---
+++ src/com/bridgelabz/functional/Factor.java
@@ -0,0 +1,18 @@
+package com.bridgelabz.functional;
+import com.bridgelabz.utility.Utility;
+public class Factor 
+{
+
+	 
+	
+		public static void main(String[] args) 
+		{
+		    System.out.println("enter number");
+			int number=Utility.integerInput();
+			System.out.println("Number"+number);
+		    Utility.factor(number);
+		
+	}
+
+
+}
